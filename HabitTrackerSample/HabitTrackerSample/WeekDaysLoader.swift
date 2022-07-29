958216b421477645ce3c6bc0438b4eea08c2be71
babaev-islom <babaev.iislom@gmail.com>
2022-07-29 16:46:20 +0500

    Add `WeekDaysLoader` service abstraction which enables to fetch `WeekDays`

---
+++ HabitTrackerSample/HabitTrackerSample/WeekDaysLoader.swift
@@ -0,0 +1,12 @@
+//
+//  WeekDaysLoader.swift
+//  HabitTrackerSample
+//
+//  Created by Islom Babaev on 29/07/22.
+//
+
+import Foundation
+
+protocol WeekDaysLoader {
+    func loadDays() -> [WeekDay]
+}
