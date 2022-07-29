9148a4fbd17b2c5ce37e601d5d3ba3d634d32a4a
babaev-islom <babaev.iislom@gmail.com>
2022-07-29 16:43:37 +0500

    Add `WeekDayType` enum for representing different week day types (past week, today, this week or next week)

---
+++ HabitTrackerSample/HabitTrackerSample/WeekDayType.swift
@@ -0,0 +1,15 @@
+//
+//  WeekType.swift
+//  HabitTrackerSample
+//
+//  Created by Islom Babaev on 29/07/22.
+//
+
+import Foundation
+
+enum WeekDayType {
+    case pastWeek(Date)
+    case today(Date)
+    case thisWeek(Date)
+    case nextWeek(Date)
+}
