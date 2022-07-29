bf820af6f831269e98ddf5cf00bf5a127bd31153
babaev-islom <babaev.iislom@gmail.com>
2022-07-29 17:05:57 +0500

    Add `DateGenerator` interface for generating `Date` objects from `Calendar`

--- HabitTrackerSample/HabitTrackerSampleTests/CalendarDaysLoaderTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/CalendarDaysLoaderTests.swift
@@ -8,7 +8,18 @@
 import XCTest
 @testable import HabitTrackerSample
 
+protocol DateGenerator {
+    func generateDates() -> [Date]
+}
+
 final class CalendarDaysLoader: WeekDaysLoader {
+    
+    private let generator: DateGenerator
+    
+    init(generator: DateGenerator) {
+        self.generator = generator
+    }
+    
     func loadDays() -> [WeekDay] {
         []
     }
@@ -18,11 +29,19 @@ final class CalendarDaysLoader: WeekDaysLoader {
 final class CalendarDaysLoaderTests: XCTestCase {
     
     func test_loadDays_doesNotLoadDays() {
-        let sut = CalendarDaysLoader()
+        let generator = StubDateGenerator()
+        let sut = CalendarDaysLoader(generator: generator)
         
         let result = sut.loadDays()
         
         XCTAssertEqual(result, [])
     }
+    
+    private class StubDateGenerator: DateGenerator {
+        func generateDates() -> [Date] {
+            []
+        }
+    }
+    
     
 }
