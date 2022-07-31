3355efaf6ceb4b49f67411737a8f04e66e7b0389
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 14:05:35 +0500

    Move `DayViewModel` into its own file in the production

--- HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
@@ -8,66 +8,6 @@
 import XCTest
 @testable import HabitTrackerSample
 
-final class DayViewModel {
-    static func map(
-        weekDays: [WeekDay],
-        timeZone: TimeZone,
-        locale: Locale,
-        calendar: Calendar
-    ) -> [PresentableDayModel] {
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeZone = timeZone
-        dateFormatter.locale = locale
-        dateFormatter.calendar = calendar
-        dateFormatter.dateFormat = "dd"
-        
-        var models = [PresentableDayModel]()
-        weekDays.forEach { day in
-            let values = mapWeekDay(day.type, calendar: calendar, dateFormatter: dateFormatter)
-            models.append(
-                PresentableDayModel(
-                    dayOfTheWeek: values.dayOfTheWeek,
-                    dateOfTheMonth: values.dateOfTheMonth,
-                    dateState: values.state
-                )
-            )
-        }
-        return models
-    }
-    
-    private static func mapWeekDay(
-        _ type: WeekDayType,
-        calendar: Calendar,
-        dateFormatter: DateFormatter
-    ) -> (dayOfTheWeek: String, dateOfTheMonth: String, state: DateState) {
-        var dayOfTheWeekString: String
-        var dateOfTheMonthString: String
-        var dateStateType: DateState
-        
-        switch type {
-        case let .inThePast(date):
-            let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
-            dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
-            dateOfTheMonthString = dateFormatter.string(from: date)
-            dateStateType = .inThePast
-            
-        case let .today(date):
-            let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
-            dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
-            dateOfTheMonthString = dateFormatter.string(from: date)
-            dateStateType = .today
-            
-        case let .inTheFuture(date):
-            let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
-            dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
-            dateOfTheMonthString = dateFormatter.string(from: date)
-            dateStateType = .inTheFuture
-        }
-        
-        return (dayOfTheWeekString, dateOfTheMonthString, dateStateType)
-    }
-}
-
 class DayViewModelTests: XCTestCase {
     private let timeZone = TimeZone(identifier: "GMT")!
     private let locale = Locale(identifier: "en_US_POSIX")
