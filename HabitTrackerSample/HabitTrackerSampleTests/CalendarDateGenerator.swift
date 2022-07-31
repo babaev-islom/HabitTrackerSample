a7483928565e5c02fb64f65198bd6122b7d6e8a1
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 12:37:08 +0500

    Generating dates with the interval of the same date (start and end) results in empty list of dates

--- HabitTrackerSample/HabitTrackerSampleTests/CalendarDateGenerator.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/CalendarDateGenerator.swift
@@ -9,16 +9,48 @@ import XCTest
 @testable import HabitTrackerSample
 
 class CalendarDateGenerator: DateGenerator {
+    private let interval: () -> DateInterval
+    private let calendar: Calendar
+    
+    init(interval: @escaping () -> DateInterval, calendar: Calendar) {
+        self.interval = interval
+        self.calendar = calendar
+    }
+    
     func generateDates() -> [Date] {
-        []
+        var dates = [Date]()
+        var start = interval().start
+        let end = interval().end
+        guard start != end else { return [] }
+        
+        while start.compare(end) != .orderedDescending {
+            dates.append(start)
+            guard let nextDay = calendar.date(byAdding: .day, value: 1, to: start) else {
+                continue
+            }
+            start = nextDay
+        }
+        return dates
     }
     
     
 }
 
 final class CalendarDateGeneratorTests: XCTestCase {
-    func test_generateDates_loadsEmptyDates() {
-        let sut = CalendarDateGenerator()
+    private lazy var calendar: Calendar = {
+        var calendar = Calendar(identifier: .iso8601)
+        calendar.timeZone = TimeZone(identifier: "GMT")!
+        return calendar
+    }()
+    
+    func test_generateDates_withTheSameDateIntervalForEndAndStart_loadsEmptyDates() {
+        let july18 = Date(timeIntervalSince1970: 1658102400)
+        let intervalWithTheSameStartAndEndDates = DateInterval(start: july18, end: july18)
+
+        let sut = CalendarDateGenerator(
+            interval: { intervalWithTheSameStartAndEndDates },
+            calendar: calendar
+        )
         
         let result = sut.generateDates()
         
