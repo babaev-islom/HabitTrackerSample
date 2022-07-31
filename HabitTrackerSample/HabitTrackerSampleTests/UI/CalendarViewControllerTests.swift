53e701b507d52bb3eabfe637172dc5d3325eeb30
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 15:56:15 +0500

    Controller loads 1 cell on 1 week day loaded

--- HabitTrackerSample/HabitTrackerSampleTests/UI/CalendarViewControllerTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/UI/CalendarViewControllerTests.swift
@@ -8,18 +8,75 @@
 import XCTest
 @testable import HabitTrackerSample
 
+final class DayCellController {
+    init(model: PresentableDayModel) {
+        
+    }
+}
+
+final class SectionCellController: NSObject {
+    private let dayCellControllers: [DayCellController]
+    
+    init(dayCellControllers: [DayCellController]) {
+        self.dayCellControllers = dayCellControllers
+    }
+}
+
+extension SectionCellController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        dayCellControllers.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+}
+extension SectionCellController: UICollectionViewDelegate {}
+extension SectionCellController: UICollectionViewDelegateFlowLayout {}
+
+final class CalendarLoaderAdapter {
+    private let timeZone: TimeZone
+    private let locale: Locale
+    private let calendar: Calendar
+    
+    init(timeZone: TimeZone, locale: Locale, calendar: Calendar) {
+        self.timeZone = timeZone
+        self.locale = locale
+        self.calendar = calendar
+    }
+
+    func mapWeekDays(weekDays: [WeekDay]) -> [SectionCellController] {
+        let presentableModels = DayViewModel.map(weekDays: weekDays, timeZone: timeZone, locale: locale, calendar: calendar)
+        let cellControllers = presentableModels.map(DayCellController.init)
+        return [
+            SectionCellController(
+                dayCellControllers: cellControllers
+            )
+        ]
+    }
+}
+
 final class CalendarFactory {
     static func makeCalendarViewController(loader: WeekDaysLoader) -> CalendarViewController {
-        return CalendarViewController(cellControllers: [])
+        let days = loader.loadDays()
+        let adapter = CalendarLoaderAdapter(
+            timeZone: TimeZone(identifier: "GMT")!,
+            locale: Locale(identifier: "en_US_POSIX"),
+            calendar: Calendar(identifier: .gregorian)
+        )
+        let cellControllers = adapter.mapWeekDays(weekDays: days)
+        return CalendarViewController(cellControllers: cellControllers)
     }
 }
 
 final class CalendarViewController: UIViewController {
     typealias CellController = UICollectionViewDataSource & UICollectionViewDelegate & UICollectionViewDelegateFlowLayout
     
-    let collectionView: UICollectionView = {
+    private(set) lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.delegate = self
+        collection.dataSource = self
         return collection
     }()
     
@@ -33,12 +90,33 @@ final class CalendarViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }
 
+extension CalendarViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellControllers[section].collectionView(collectionView, numberOfItemsInSection: section)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        UICollectionViewCell()
+    }
+}
+
+extension CalendarViewController: UICollectionViewDelegate {}
+
+
 final class CalendarViewControllerTests: XCTestCase {
+    
     func test_viewDidLoad_doesNotRenderDays() {
-        let stub = WeekDaysLoaderStub()
-        let sut = CalendarFactory.makeCalendarViewController(loader: stub)
+        let loader = WeekDaysLoaderStub()
+        loader.stub(with: [])
+
+        let sut = CalendarFactory.makeCalendarViewController(loader: loader)
             
         sut.loadViewIfNeeded()
         
@@ -46,9 +124,29 @@ final class CalendarViewControllerTests: XCTestCase {
     }
     
     
+    func test_viewDidLoad_withOneCell_rendersOneDay() {
+        let loader = WeekDaysLoaderStub()
+        loader.stub(with: [WeekDay(type: .today(Date()))])
+        
+        let sut = CalendarFactory.makeCalendarViewController(loader: loader)
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(sut.numberOfRenderedDays(), 1)
+    }
+    
+    
     private class WeekDaysLoaderStub: WeekDaysLoader {
+        private var _stub: [WeekDay]?
+        func stub(with days: [WeekDay]) {
+            self._stub = days
+        }
+        
         func loadDays() -> [WeekDay] {
-            []
+            guard let stub = _stub else {
+                fatalError("Stub the loader before using")
+            }
+            
+            return stub
         }
     }
 
