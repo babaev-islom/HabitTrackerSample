152373c3033fe16d0f659a04758335562757f5ee
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 13:18:30 +0500

    Mapping weekdays with 1 week day results in a single presentable model

--- HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
@@ -8,13 +8,33 @@
 import XCTest
 @testable import HabitTrackerSample
 
+enum DateState {
+    case today
+}
+
 struct PresentableDayModel {
-    
+    let dayOfTheWeek: String
+    let dateOfTheMonth: String
+    let dateState: DateState
 }
 
 final class DayViewModel {
     static func map(weekDays: [WeekDay]) -> [PresentableDayModel] {
-        []
+        var models = [PresentableDayModel]()
+        if let firstWeekDay = weekDays.first {
+            switch firstWeekDay.type {
+            case .today:
+                models.append(
+                    PresentableDayModel(
+                        dayOfTheWeek: "Mon",
+                        dateOfTheMonth: "18",
+                        dateState: .today
+                    )
+                )
+            default: break
+            }
+        }
+        return models
     }
 }
 
@@ -23,5 +43,17 @@ class DayViewModelTests: XCTestCase {
         let result = DayViewModel.map(weekDays: [])
         
         XCTAssertTrue(result.isEmpty)
+    }
+    
+    func test_map_withWeekDayTodayTypeAndSpecificDate_resultsWithMappedModelWithTodayState() {
+        let july18 = Date(timeIntervalSince1970: 1658102400)
+        
+        let weekDay = WeekDay(type: .today(july18))
+        let result = DayViewModel.map(weekDays: [weekDay])
+        
+        XCTAssertEqual(result.count, 1)
+        XCTAssertEqual(result.first?.dayOfTheWeek, "Mon")
+        XCTAssertEqual(result.first?.dateOfTheMonth, "18")
+        XCTAssertEqual(result.first?.dateState, .today)
     }
 }
