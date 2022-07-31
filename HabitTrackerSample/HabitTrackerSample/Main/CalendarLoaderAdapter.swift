115ea6f9d30eeac662908da38f776dd9545b8c52
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 19:53:07 +0500

    Extract adapter into its own file

---
+++ HabitTrackerSample/HabitTrackerSample/Main/CalendarLoaderAdapter.swift
@@ -0,0 +1,39 @@
+//
+//  CalendarLoaderAdapter.swift
+//  HabitTrackerSample
+//
+//  Created by Islom Babaev on 31/07/22.
+//
+
+import Foundation
+
+final class CalendarLoaderAdapter {
+    private let timeZone: TimeZone
+    private let locale: Locale
+    private let calendar: Calendar
+    
+    init(timeZone: TimeZone, locale: Locale, calendar: Calendar) {
+        self.timeZone = timeZone
+        self.locale = locale
+        self.calendar = calendar
+    }
+
+    func mapWeekDays(weekDays: [WeekDay]) -> [SectionCellController] {
+        let presentableModels = DayViewModel.map(weekDays: weekDays, timeZone: timeZone, locale: locale, calendar: calendar)
+        let cellControllers = presentableModels.map(DayCellController.init)
+        return [
+            SectionCellController(
+                dayCellControllers: cellControllers,
+                selectedCellIndex: findTodayModel(in: presentableModels)
+            )
+        ]
+    }
+    
+    private func findTodayModel(in models: [PresentableDayModel]) -> Int {
+        var selectedIndex = 0
+        models.enumerated().forEach { index, model in
+            if model.dateState == .today { selectedIndex = index }
+        }
+        return selectedIndex
+    }
+}
