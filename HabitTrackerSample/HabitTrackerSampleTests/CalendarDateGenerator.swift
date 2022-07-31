a5f66631a413dbd247b17eb70f8859d7bc839dfd
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 13:01:35 +0500

    Move `CalendarDateGenerato` into its own file in the production

--- HabitTrackerSample/HabitTrackerSampleTests/CalendarDateGenerator.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/CalendarDateGenerator.swift
@@ -8,38 +8,7 @@
 import XCTest
 @testable import HabitTrackerSample
 
-class CalendarDateGenerator: DateGenerator {
-    private let interval: () -> DateInterval
-    private let calendar: Calendar
-    
-    init(interval: @escaping () -> DateInterval, calendar: Calendar) {
-        self.interval = interval
-        self.calendar = calendar
-    }
-    
-    func generateDates() -> [Date] {
-        let start = interval().start
-        let end = interval().end
-        guard start != end else { return [] }
-        
-        return generateDates(from: start, to: end)
-    }
-    
-    private func generateDates(from startDate: Date, to endDate: Date) -> [Date] {
-        var dates = [Date]()
-        var mutableStartDate = startDate
-        while mutableStartDate.compare(endDate) != .orderedDescending {
-            dates.append(mutableStartDate)
-            guard let nextDay = calendar.date(byAdding: .day, value: 1, to: mutableStartDate) else {
-                continue
-            }
-            mutableStartDate = nextDay
-        }
-        return dates
-    }
-    
-    
-}
+
 
 final class CalendarDateGeneratorTests: XCTestCase {
     
