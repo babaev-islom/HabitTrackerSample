98d73f43298c0a2cab194e40606e72d92912f5b6
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 15:58:44 +0500

    Move `CalendarViewController` to its own file in the production

--- HabitTrackerSample/HabitTrackerSampleTests/UI/CalendarViewControllerTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/UI/CalendarViewControllerTests.swift
@@ -69,45 +69,7 @@ final class CalendarFactory {
     }
 }
 
-final class CalendarViewController: UIViewController {
-    typealias CellController = UICollectionViewDataSource & UICollectionViewDelegate & UICollectionViewDelegateFlowLayout
-    
-    private(set) lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collection.delegate = self
-        collection.dataSource = self
-        return collection
-    }()
-    
-    private let cellControllers: [CellController]
-    
-    init(cellControllers: [CellController]) {
-        self.cellControllers = cellControllers
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-}
 
-extension CalendarViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cellControllers[section].collectionView(collectionView, numberOfItemsInSection: section)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        UICollectionViewCell()
-    }
-}
-
-extension CalendarViewController: UICollectionViewDelegate {}
 
 
 final class CalendarViewControllerTests: XCTestCase {
