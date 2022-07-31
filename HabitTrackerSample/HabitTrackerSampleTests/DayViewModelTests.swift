2e8680655e10521c1955d0c53176431465c5201c
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 13:53:09 +0500

    Mapping three different week days with different types results into a correctly mapped presentable model with respective date state

--- HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
@@ -11,6 +11,7 @@ import XCTest
 enum DateState {
     case today
     case inThePast
+    case inTheFuture
 }
 
 struct PresentableDayModel {
@@ -33,13 +34,17 @@ final class DayViewModel {
         dateFormatter.dateFormat = "dd"
         
         var models = [PresentableDayModel]()
-        if let firstWeekDay = weekDays.first {
-            switch firstWeekDay.type {
-            case .today:
+        weekDays.forEach { day in
+            switch day.type {
+            case let .today(date):
+                let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
+                let dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
+                let dayOfTheMonth = dateFormatter.string(from: date)
+
                 models.append(
                     PresentableDayModel(
-                        dayOfTheWeek: "Mon",
-                        dateOfTheMonth: "18",
+                        dayOfTheWeek: dayOfTheWeekString,
+                        dateOfTheMonth: dayOfTheMonth,
                         dateState: .today
                     )
                 )
@@ -55,7 +60,18 @@ final class DayViewModel {
                         dateState: .inThePast
                     )
                 )
-            default: break
+            case let .inTheFuture(date):
+                let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
+                let dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
+                let dayOfTheMonth = dateFormatter.string(from: date)
+
+                models.append(
+                    PresentableDayModel(
+                        dayOfTheWeek: dayOfTheWeekString,
+                        dateOfTheMonth: dayOfTheMonth,
+                        dateState: .inTheFuture
+                    )
+                )
             }
         }
         return models
@@ -105,5 +121,39 @@ class DayViewModelTests: XCTestCase {
         XCTAssertEqual(result.first?.dayOfTheWeek, "Tue")
         XCTAssertEqual(result.first?.dateOfTheMonth, "19")
         XCTAssertEqual(result.first?.dateState, .inThePast)
+    }
+    
+    func test_map_threeWeekDays_resultsInThreePresentableModels() {
+        let july19 = Date(timeIntervalSince1970: 1658188800)
+        let july20 = Date(timeIntervalSince1970: 1658275200)
+        let july21 = Date(timeIntervalSince1970: 1658361600)
+
+        let weekDayToday = WeekDay(type: .today(july19))
+        let weekDayInThePast = WeekDay(type: .inThePast(july20))
+        let weekDayInTheFuture = WeekDay(type: .inTheFuture(july21))
+
+        let result = DayViewModel.map(
+            weekDays: [
+                weekDayToday,
+                weekDayInThePast,
+                weekDayInTheFuture
+            ],
+            timeZone: timeZone,
+            locale: locale,
+            calendar: calendar
+        )
+
+        XCTAssertEqual(result.count, 3)
+        XCTAssertEqual(result[0].dayOfTheWeek, "Tue", "Expected July 19 to be Tuesday")
+        XCTAssertEqual(result[0].dateOfTheMonth, "19", "Expected July 19 Date to be mapped into 19")
+        XCTAssertEqual(result[0].dateState, .today, "Expected today type")
+        
+        XCTAssertEqual(result[1].dayOfTheWeek, "Wed", "Expected July 20 to be Wednesday")
+        XCTAssertEqual(result[1].dateOfTheMonth, "20", "Expected July 20 Date to be mapped into 20")
+        XCTAssertEqual(result[1].dateState, .inThePast, "Expected inThePast type")
+        
+        XCTAssertEqual(result[2].dayOfTheWeek, "Thu", "Expected July 21 to be Thursday")
+        XCTAssertEqual(result[2].dateOfTheMonth, "21", "Expected July 21 to be mapped into 21")
+        XCTAssertEqual(result[2].dateState, .inTheFuture, "Expected inTheFuture type")
     }
 }
