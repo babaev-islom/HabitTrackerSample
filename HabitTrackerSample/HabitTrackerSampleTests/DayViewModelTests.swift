de96ac700063d1f4d3f2c9c9ddd1368df328c5f3
babaev-islom <babaev.iislom@gmail.com>
2022-07-31 14:02:54 +0500

    Refactor duplicate logic into helper

--- HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
+++ HabitTrackerSample/HabitTrackerSampleTests/DayViewModelTests.swift
@@ -35,46 +35,48 @@ final class DayViewModel {
         
         var models = [PresentableDayModel]()
         weekDays.forEach { day in
-            switch day.type {
-            case let .today(date):
-                let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
-                let dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
-                let dayOfTheMonth = dateFormatter.string(from: date)
-
-                models.append(
-                    PresentableDayModel(
-                        dayOfTheWeek: dayOfTheWeekString,
-                        dateOfTheMonth: dayOfTheMonth,
-                        dateState: .today
-                    )
+            let values = mapWeekDay(day.type, calendar: calendar, dateFormatter: dateFormatter)
+            models.append(
+                PresentableDayModel(
+                    dayOfTheWeek: values.dayOfTheWeek,
+                    dateOfTheMonth: values.dateOfTheMonth,
+                    dateState: values.state
                 )
-            case let .inThePast(date):
-                let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
-                let dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
-                let dayOfTheMonth = dateFormatter.string(from: date)
-
-                models.append(
-                    PresentableDayModel(
-                        dayOfTheWeek: dayOfTheWeekString,
-                        dateOfTheMonth: dayOfTheMonth,
-                        dateState: .inThePast
-                    )
-                )
-            case let .inTheFuture(date):
-                let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
-                let dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
-                let dayOfTheMonth = dateFormatter.string(from: date)
-
-                models.append(
-                    PresentableDayModel(
-                        dayOfTheWeek: dayOfTheWeekString,
-                        dateOfTheMonth: dayOfTheMonth,
-                        dateState: .inTheFuture
-                    )
-                )
-            }
+            )
         }
         return models
+    }
+    
+    private static func mapWeekDay(
+        _ type: WeekDayType,
+        calendar: Calendar,
+        dateFormatter: DateFormatter
+    ) -> (dayOfTheWeek: String, dateOfTheMonth: String, state: DateState) {
+        var dayOfTheWeekString: String
+        var dateOfTheMonthString: String
+        var dateStateType: DateState
+        
+        switch type {
+        case let .inThePast(date):
+            let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
+            dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
+            dateOfTheMonthString = dateFormatter.string(from: date)
+            dateStateType = .inThePast
+            
+        case let .today(date):
+            let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
+            dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
+            dateOfTheMonthString = dateFormatter.string(from: date)
+            dateStateType = .today
+            
+        case let .inTheFuture(date):
+            let dayOfTheWeek = calendar.component(.weekday, from: date) - 1
+            dayOfTheWeekString = dateFormatter.shortWeekdaySymbols[dayOfTheWeek]
+            dateOfTheMonthString = dateFormatter.string(from: date)
+            dateStateType = .inTheFuture
+        }
+        
+        return (dayOfTheWeekString, dateOfTheMonthString, dateStateType)
     }
 }
 
