fdafed4ce003c92c880a256464f3a7640ad19baa
babaev-islom <babaev.iislom@gmail.com>
2022-08-01 11:04:39 +0500

    Extract selected index fetching into constructor injection

--- HabitTrackerSample/HabitTrackerSample/Main/CalendarFactory.swift
+++ HabitTrackerSample/HabitTrackerSample/Main/CalendarFactory.swift
@@ -7,44 +7,15 @@
 
 import Foundation
 
-final class CalendarLoaderAdapter {
-    private let timeZone: TimeZone
-    private let locale: Locale
-    private let calendar: Calendar
-    
-    init(timeZone: TimeZone, locale: Locale, calendar: Calendar) {
-        self.timeZone = timeZone
-        self.locale = locale
-        self.calendar = calendar
-    }
-
-    func mapWeekDays(weekDays: [WeekDay]) -> [SectionCellController] {
-        let presentableModels = DayViewModel.map(weekDays: weekDays, timeZone: timeZone, locale: locale, calendar: calendar)
-        let cellControllers = presentableModels.map(DayCellController.init)
-        return [
-            SectionCellController(
-                dayCellControllers: cellControllers,
-                selectedCellIndex: findTodayModel(in: presentableModels)
-            )
-        ]
-    }
-    
-    private func findTodayModel(in models: [PresentableDayModel]) -> Int {
-        var selectedIndex = 0
-        models.enumerated().forEach { index, model in
-            if model.dateState == .today { selectedIndex = index }
-        }
-        return selectedIndex
-    }
-}
-
 final class CalendarFactory {
     static func makeCalendarViewController(loader: WeekDaysLoader) -> CalendarViewController {
         let days = loader.loadDays()
+        let selectedIndex = TodayModelSearchManager.findTodayModel(in: days)
         let adapter = CalendarLoaderAdapter(
             timeZone: TimeZone(identifier: "GMT")!,
             locale: Locale(identifier: "en_US_POSIX"),
-            calendar: Calendar(identifier: .gregorian)
+            calendar: Calendar(identifier: .gregorian),
+            selectedIndex: selectedIndex
         )
         let cellControllers = adapter.mapWeekDays(weekDays: days)
         let controller = CalendarViewController(
--- HabitTrackerSample/HabitTrackerSample/Main/CalendarLoaderAdapter.swift
+++ HabitTrackerSample/HabitTrackerSample/Main/CalendarLoaderAdapter.swift
@@ -11,11 +11,18 @@ final class CalendarLoaderAdapter {
     private let timeZone: TimeZone
     private let locale: Locale
     private let calendar: Calendar
+    private let selectedIndex: Int
     
-    init(timeZone: TimeZone, locale: Locale, calendar: Calendar) {
+    init(
+        timeZone: TimeZone,
+        locale: Locale,
+        calendar: Calendar,
+        selectedIndex: Int
+    ) {
         self.timeZone = timeZone
         self.locale = locale
         self.calendar = calendar
+        self.selectedIndex = selectedIndex
     }
 
     func mapWeekDays(weekDays: [WeekDay]) -> [SectionCellController] {
@@ -24,7 +31,7 @@ final class CalendarLoaderAdapter {
         return [
             SectionCellController(
                 dayCellControllers: cellControllers,
-                selectedCellIndex: TodayModelSearchManager.findTodayModel(in: weekDays)
+                selectedCellIndex: selectedIndex
             )
         ]
     }
